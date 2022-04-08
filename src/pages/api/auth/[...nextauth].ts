9b211a867631eb4636c842547854ba6b80f55b39
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-04-07 23:48:02 -0300

    feat#Validando assinatura ativa

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
 import { query as q } from "faunadb";
-
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-
 import { fauna } from "../../../services/fauna";
+
+
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -21,6 +21,43 @@ export default NextAuth({
   ],
 
   callbacks: {
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        );
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+    },
+    
     async signIn({ user }) {
       const { email } = user;
 
